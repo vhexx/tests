ea4582de8d8196ea70e6edaef8f3e0fc3cc074a8
Viktor <vhex@yandex.ru>
2015-06-20 02:46:11 +0300

    create models

--- tests/models.py
+++ tests/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
 
+class TestPrototype:
+    title = models.CharField(max_length=100)
+
+
+class QuestionPrototype:
+    title = models.CharField(max_length=200)
+    test = models.ForeignKey(TestPrototype)
+
+
+class AnswerPrototype:
+    question = models.ForeignKey(QuestionPrototype)
+    statement = models.CharField(max_length=300, null=True)
+    image = models.ForeignKey(ImagePrototype, null=True)
+
+
+class ImagePrototype:
+    img = models.ImageField()

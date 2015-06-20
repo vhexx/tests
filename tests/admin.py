6ea3b3e07be21b09cc90d51c040b0f1e6949c0a1
unknown <profgansmok@gmail.com>
2015-06-20 19:04:01 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -4,17 +4,4 @@ from nested_inline.admin import NestedStackedInline, NestedModelAdmin
 
 class AnswerInline(admin.StackedInline):
     model = AnswerPrototype
-    extra = 2
-    fk_name = 'question'
-
-class QuestionInline(admin.StackedInline):
-    model = QuestionPrototype
-    extra = 1
-    fk_name = 'test'
-    inlines = [AnswerInline]
-
-class TestAdmin(admin.ModelAdmin):
-    model = TestPrototype
-    inlines = [QuestionInline]
-
-admin.site.register(TestPrototype, TestAdmin)
+   fdfhfdh

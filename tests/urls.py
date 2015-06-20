ce4d63864a41360a3fab586596ddb840165ba292
viktorhex <vhex@yandex.ru>
2015-06-20 23:27:11 +0300

    fix routing

--- tests/urls.py
+++ tests/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from tests import views
 
 urlpatterns = patterns('',
-    url(r'^admin', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^test$', views.test),
     url(r'^question$', views.question),
     url(r'^$', views.index),

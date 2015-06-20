6545341cd6167be3108988d0b56a59123e52ac7a
unknown <profgansmok@gmail.com>
2015-06-20 23:42:06 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import TestPrototype, QuestionPrototype, AnswerPrototype, ImagePrototype
+from django.http import HttpResponseRedirect
 
 class TestAdmin(admin.ModelAdmin):
     model = TestPrototype

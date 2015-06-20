9ad5639b57e31971ad209f05ab35f51701cd69b6
viktorhex <vhex@yandex.ru>
2015-06-20 17:43:13 +0300

    update model representation

--- tests/views.py
+++ tests/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render_to_response
 
 
 def test(request):
-    render_to_response('test.html')
+    return render_to_response('test.html')
 
 
 def question(request):
-    render_to_response('question.html')
+    return render_to_response('question.html')

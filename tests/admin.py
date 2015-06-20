af46e2375317eb6dee624c31dbdb618ee4269bad
unknown <profgansmok@gmail.com>
2015-06-20 19:05:55 +0300

    cmt

--- tests/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TestPrototype, QuestionPrototype, AnswerPrototype, ImagePrototype
-from nested_inline.admin import NestedStackedInline, NestedModelAdmin
-
-class AnswerInline(admin.StackedInline):
-    model = AnswerPrototype
-   fdfhfdh

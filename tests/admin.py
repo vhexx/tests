8705abc71b72b3736852b3eb8711e97b73b7a637
unknown <profgansmok@gmail.com>
2015-06-20 21:38:03 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import TestPrototype, QuestionPrototype, AnswerPrototype, ImagePrototype
 from django.forms import ModelForm
+from django.shortcuts import render
 
-#class QuestionAdmin(admin.ModelAdmin):
-	#model = QuestionPrototype
-	#add_form_template = 'question_form.html'
-   # def add_view(self, request, form_url="", extra_context=None):
-    #	return render(request, 'question_form.html', {'form' : self.form, 'i' : 5})
+class QuestionAdmin(admin.ModelAdmin):
+	model = QuestionPrototype
+	add_form_template = 'question_form.html'
+    def add_view(self, request, form_url="", extra_context=None):
+    	return render(request, 'question_form.html', {'form' : self.form, 'i' : 5})
 
 class QuestionInline(admin.StackedInline):
     model = QuestionPrototype
@@ -17,5 +18,5 @@ class TestAdmin(admin.ModelAdmin):
     model = TestPrototype
     inlines = [QuestionInline]
 
-#admin.site.register(QuestionPrototype, QuestionAdmin)
+admin.site.register(QuestionPrototype, QuestionAdmin)
 admin.site.register(TestPrototype, TestAdmin)

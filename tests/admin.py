39f1ea0f7096b207a24285f343eccb1952519be7
unknown <profgansmok@gmail.com>
2015-06-21 02:07:57 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -1,20 +1,34 @@
 from django.contrib import admin
 from .models import TestPrototype, QuestionPrototype, AnswerPrototype, ImagePrototype
+from django.forms import ModelForm
 from django.http import HttpResponseRedirect
 
 class AnswerInline(admin.StackedInline):
 	model = AnswerPrototype
 
+class QuestionForm(ModelForm):
+	class Meta:
+		model = QuestionPrototype
+	test_id = None
+	def __init__(self, *args, **kwargs):
+		super(QuestionForm, self).__init__(*args, **kwargs)
+		if test_id:
+			self.fields['test'].initial = test_id
+
 class QuestionAdmin(admin.ModelAdmin):
 	model = QuestionPrototype
 	inlines = [AnswerInline]
-	#def add_view(self, request, obj, form, change):
-		#return super(QuestionAdmin, self).add_view(self, request, obj, form, change)
+	form = QuestionForm
+	def add_view(self, request, obj, form, change):
+		if test_id in request:
+			QuestionAdmin.form.test_id = int(request.test_id)
+		return super(QuestionAdmin, self).add_view(self, request, obj, form, change)
+    
 
 class TestAdmin(admin.ModelAdmin):
     model = TestPrototype
     def response_add(self, request, obj, post_url_continue=None):
-    	return HttpResponseRedirect('../../questionprototype?test_id='+str(obj.id))
+    	return HttpResponseRedirect('../../questionprototype/add/?test_id='+str(obj.id))
 
 admin.site.register(QuestionPrototype, QuestionAdmin)
 admin.site.register(TestPrototype, TestAdmin)

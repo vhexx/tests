7c7f87aa9fc65a1986341bc1cf646709e4c05015
Viktor <vhex@yandex.ru>
2015-06-20 03:37:48 +0300

    register models in admin.py

--- tests/admin.py
+++ tests/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from .models import TestPrototype, QuestionPrototype, AnswerPrototype, ImagePrototype
 
 def action1 (modeladmin, request, queryset):
     queryset.update(first_name='ivan')
 action1.short_description = "action 1"
+
+admin.site.register(TestPrototype)
+admin.site.register(QuestionPrototype)
+admin.site.register(AnswerPrototype)
+admin.site.register(ImagePrototype)

ac5ca195d923419019965d5e0b6522ff695c788a
Viktor <vhex@yandex.ru>
2015-06-20 18:24:33 +0300

    update test view

--- tests/views.py
+++ tests/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render_to_response
+from tests.models import TestPrototype
 
 
 def test(request):
-    return render_to_response('test.html')
+    test_id = request.GET.get('id')
+    test_instance = TestPrototype.objects.get(id=test_id)
+    context = {
+        'test_title': test_instance.title
+    }
+    return render_to_response('test.html', context)
 
 
 def question(request):
+    id = request.GET.get('id')
     return render_to_response('question.html')

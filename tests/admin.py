85f19f3b74c3a3776b78ee09a4da2b1cddf8b9f5
unknown <profgansmok@gmail.com>
2015-06-20 18:15:21 +0300

    change admin.py

---
+++ tests/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import TestPrototype, QuestionPrototype, AnswerPrototype, ImagePrototype
+#from django import forms
+#from django.shortcuts import render, render_to_response
+#from django.http import HttpResponseRedirect
+
+
+#class addQuestionsForm(forms.Form):
+    #message = forms.CharField(max_length=100)
+
+
+#def add_questions(modeladmin, request, queryset):
+    #form = None
+    #if request.method == 'POST':
+       #form = addQuestionsForm(request.POST)
+       #if form.is_valid():
+           # message = form.cleaned_data['message']
+            #modeladmin.message_user(request, "%s !!!" % message)
+            #return HttpResponseRedirect('admin/admin_base.html')
+    #if not form:
+        #form = addQuestionsForm()
+    #return render_to_response('admin/addquestions.html', {'form': form})
+
+class AnswerInline(admin.StackedInline):
+    model = AnswerPrototype
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+class QuestionInline(admin.StackedInline):
+    model = QuestionPrototype
+
+
+class TestAdmin(admin.ModelAdmin):
+    actions = [add_questions]
+    #inlines = [QuestionInline]
+
+admin.site.register(QuestionPrototype, QuestionAdmin)
+admin.site.register(TestPrototype, TestAdmin)

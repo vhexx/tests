17c51190714c31f9c5c6e2cb4e4a09b5b5e64cb9
viktorhex <vhex@yandex.ru>
2015-06-20 03:08:35 +0300

    fix demo model

--- tests/models.py
+++ tests/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 
-class TestPrototype:
+class TestPrototype(models.Model):
     title = models.CharField(max_length=100)
 
 
-class QuestionPrototype:
+class QuestionPrototype(models.Model):
     title = models.CharField(max_length=200)
     test = models.ForeignKey(TestPrototype)
 
 
-class AnswerPrototype:
+class AnswerPrototype(models.Model):
     question = models.ForeignKey(QuestionPrototype)
     statement = models.CharField(max_length=300, null=True)
     image = models.ForeignKey(ImagePrototype, null=True)
 
 
-class ImagePrototype:
+class ImagePrototype(models.Model):
     img = models.ImageField()

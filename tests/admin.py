6314dc72de9369d48761d094d54eefa6f05b0c1e
viktorhex <vhex@yandex.ru>
2015-06-20 03:12:43 +0300

    fix admin.py

--- tests/admin.py
+++ tests/admin.py
@@ -1,13 +1,5 @@
 from django.contrib import admin
-from tests.models import Person
 
 def action1 (modeladmin, request, queryset):
     queryset.update(first_name='ivan')
 action1.short_description = "action 1"
-
-class PersonAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name']
-    ordering = ['last_name']
-    actions = [action1]
-
-admin.site.register(Person, PersonAdmin)

5de2ecc5fea641d6016134b262b3a41799815337
Viktor <vhex@yandex.ru>
2015-06-20 22:48:05 +0300

    update views for question

---
+++ tests/utils/serialize.py
@@ -0,0 +1,11 @@
+# gets question QuerySet and returns serialized string
+def serialize_questions(question_set):
+    question_string = ''
+    for q in question_set:
+        question_string = question_string + q.id + ';'
+    return question_string[:-1]
+
+# gets serialized string and resturns list of ids(int)
+def deserialize_questions(question_string):
+    return list(map(int, question_string.split(';')))
+

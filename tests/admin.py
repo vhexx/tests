aba00de7afe9f8e7d5dc6156060f6e5b863e15a7
unknown <profgansmok@gmail.com>
2015-06-21 01:45:30 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from .models import TestPrototype, QuestionPrototype, AnswerPrototype, ImagePrototype
 from django.http import HttpResponseRedirect
 
+class AnswerInline(admin.StackedInline):
+	model = AnswerPrototype
+
+class QuestionAdmin(admin.ModelAdmin):
+	model = QuestionPrototype
+	inlines = [AnswerInline]
+	#def add_view(self, request, obj, form, change):
+		#return super(QuestionAdmin, self).add_view(self, request, obj, form, change)
+
 class TestAdmin(admin.ModelAdmin):
     model = TestPrototype
     def response_add(self, request, obj, post_url_continue=None):
-    	#if form.is_valid():
-    	return HttpResponseRedirect('../../questionprototype')
-    	#else:
-    		#return HttpResponseRedirect('../../')
+    	return HttpResponseRedirect('../../questionprototype?test_id='+obj.id)
 
+admin.site.register(QuestionPrototype, QuestionAdmin)
 admin.site.register(TestPrototype, TestAdmin)

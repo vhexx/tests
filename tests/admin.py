cffb59e51a4297f142539766738bb1f50b832139
viktorhex <vhex@yandex.ru>
2015-06-21 16:01:16 +0300

    update admin

--- tests/admin.py
+++ tests/admin.py
@@ -4,30 +4,41 @@ from django.forms import ModelForm
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
+
 class AnswerInline(admin.StackedInline):
-	model = AnswerPrototype
+    model = AnswerPrototype
+
 
 class QuestionForm(ModelForm):
-	class Meta:
-		model = QuestionPrototype
-	test_id = None
-	def __init__(self, *args, **kwargs):
-		super(QuestionForm, self).__init__(*args, **kwargs)
-		if self.test_id:
-			self.fields['test'].queryset = model.objects().filter(test=self.test_id)
+    class Meta:
+        model = QuestionPrototype
+
+    test_id = None
+
+    def __init__(self, *args, **kwargs):
+        super(QuestionForm, self).__init__(*args, **kwargs)
+        if self.test_id:
+            self.fields['test'].queryset = model.objects().filter(test=self.test_id)
+
+
 class QuestionAdmin(admin.ModelAdmin):
-	model = QuestionPrototype
-	inlines = [AnswerInline]
-	form = QuestionForm
-	def add_view(self, request, form_url='', extra_context=None):
-		form.test_id = 1 #здесь нужно получить id теста по запросу
-		return super(QuestionAdmin, self).add_view(request, form_url, extra_context)
+    model = QuestionPrototype
+    inlines = [AnswerInline]
+
+    global form
+    form = QuestionForm
+
+    def add_view(self, request, form_url='', extra_context=None):
+        form.test_id = 1  # здесь нужно получить id теста по запросу
+        return super(QuestionAdmin, self).add_view(request, form_url, extra_context)
 
 
 class TestAdmin(admin.ModelAdmin):
     model = TestPrototype
+
     def response_add(self, request, obj, post_url_continue=None):
-    	return HttpResponseRedirect('../../questionprototype/add/')#здесь нужно передать в запрос obj.test
+        return HttpResponseRedirect('../../questionprototype/add/')  # здесь нужно передать в запрос obj.test
+
 
 admin.site.register(QuestionPrototype, QuestionAdmin)
 admin.site.register(TestPrototype, TestAdmin)

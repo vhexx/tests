5c45d7349316718057b67bc9017247b2cde4a360
Viktor <victorbodrov@yandex.ru>
2015-06-21 07:27:34 +0300

    rollback admin.py

--- tests/admin.py
+++ tests/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import TestPrototype, QuestionPrototype, AnswerPrototype, ImagePrototype
 from django.forms import ModelForm
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
 
 class AnswerInline(admin.StackedInline):
 	model = AnswerPrototype
@@ -15,22 +14,19 @@ class QuestionForm(ModelForm):
 		super(QuestionForm, self).__init__(*args, **kwargs)
 		if self.test_id:
 			self.fields['test'].queryset = model.objects().filter(test=self.test_id)
-
 class QuestionAdmin(admin.ModelAdmin):
-    #global form
 	model = QuestionPrototype
 	inlines = [AnswerInline]
-    form = QuestionForm
+	form = QuestionForm
 	def add_view(self, request, form_url='', extra_context=None):
-		form.test_id = 2 #здесь нужно получить id теста по запросу
+		form.test_id = 1 #здесь нужно получить id теста по запросу
 		return super(QuestionAdmin, self).add_view(request, form_url, extra_context)
-
+    
 
 class TestAdmin(admin.ModelAdmin):
     model = TestPrototype
     def response_add(self, request, obj, post_url_continue=None):
-        return HttpResponseRedirect('/')
-    #	return HttpResponseRedirect(reverse('../../questionprototype/add/',kwargs={'test': form.test_id}))#здесь нужно передать в запрос obj.test
+    	return HttpResponseRedirect('../../questionprototype/add/')#здесь нужно передать в запрос obj.test
 
 admin.site.register(QuestionPrototype, QuestionAdmin)
 admin.site.register(TestPrototype, TestAdmin)

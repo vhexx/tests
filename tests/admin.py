9f5fedfd6b30a3a00097ad0ac884f3bd4ff0040e
unknown <profgansmok@gmail.com>
2015-06-22 02:08:16 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -8,7 +8,6 @@ from django.core.urlresolvers import reverse
 class AnswerInline(admin.StackedInline):
     model = AnswerPrototype
 
-
 class QuestionForm(ModelForm):
     class Meta:
         model = QuestionPrototype
@@ -17,7 +16,7 @@ class QuestionForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         if self.test_id:
-            self.fields['test'].queryset = self.Meta.model.objects.get(pk=self.test_id)
+            self.fields['test'].queryset = TestPrototype.objects.get(pk=self.test_id)
             self.readonly_fields = ('test',)
         super(QuestionForm, self).__init__(*args, **kwargs)
 
@@ -29,16 +28,25 @@ class QuestionAdmin(admin.ModelAdmin):
     def add_view(self, request, form_url='', extra_context=None):
         test_id = request.GET.get('test_id')
         self.form.test_id = test_id
-        #debug
         print('test_id:'+str(test_id))
         return super(QuestionAdmin, self).add_view(request, form_url, extra_context)
 
+    def response_add(self, request, obj, post_url_continue=None):
+        return HttpResponseRedirect('../../questionprototype/add?test_id=%s' % str(self.form.test_id))
+
+    def delete_model(request, obj):
+        obj.delete()
+        return super(QuestionAdmin, self).delete_model(request, obj)
 
 class TestAdmin(admin.ModelAdmin):
     model = TestPrototype
 
     def response_add(self, request, obj, post_url_continue=None):
         return HttpResponseRedirect('../../questionprototype/add?test_id=%s' % str(obj.id))
+
+    def delete_model(request, obj):
+        obj.delete()
+        return super(TestAdmin, self).delete_model(request, obj)
 
 
 admin.site.register(QuestionPrototype, QuestionAdmin)

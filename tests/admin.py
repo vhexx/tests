f62e3d09a3922ebcced17466f2ea4148bf701e37
unknown <profgansmok@gmail.com>
2015-06-22 03:56:55 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -46,14 +46,14 @@ class QuestionInline(admin.StackedInline):
 class TestAdmin(admin.ModelAdmin):
     model = TestPrototype
 
-    def change_view(request, object_id, form_url='', extra_context=None):
-        inlines = [QuestionInline]
-        return super(TestAdmin).change_view(request, object_id, form_url, extra_context)
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        self.inlines = [QuestionInline]
+        return super(TestAdmin, self).change_view(request, object_id, form_url, extra_context)
 
     def response_add(self, request, obj, post_url_continue=None):
         return HttpResponseRedirect('../../questionprototype/add?test_id=%s' % str(obj.id))
 
-    def delete_model(request, obj):
+    def delete_model(self, request, obj):
         obj.delete()
         return super(TestAdmin, self).delete_model(request, obj)
 

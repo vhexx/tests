d75c5ef2e9afea32f8597ffce36d9572090cd2fa
unknown <profgansmok@gmail.com>
2015-06-22 03:21:05 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -24,12 +24,14 @@ class QuestionForm(ModelForm):
 class QuestionAdmin(admin.ModelAdmin):
     model = QuestionPrototype
     inlines = [AnswerInline]
-    form = QuestionForm
+    #form = QuestionForm
 
     def add_view(self, request, form_url='', extra_context=None):
         test_id = request.GET.get('test_id')
-        self.form.test_id = test_id
+        #self.form.test_id = test_id
         #print('test_id:'+str(test_id))
+        if test_id:
+            self.readonly_fields = ['test']
         return super(QuestionAdmin, self).add_view(request, form_url, extra_context)
 
     def response_add(self, request, obj, post_url_continue=None):

1c47e987cf42dafaef469c97196dde752096ff36
unknown <profgansmok@gmail.com>
2015-06-24 12:48:14 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -9,35 +9,13 @@ from django.core.urlresolvers import reverse
 class AnswerInline(admin.StackedInline):
     model = AnswerPrototype
 
-class QuestionForm(ModelForm):
-    class Meta:
-        model = QuestionPrototype
-
-    test_id = None
-
-    def __init__(self, *args, **kwargs):
-        super(QuestionForm, self).__init__(*args, **kwargs)
-        if self.test_id:
-            tests_set = TestPrototype.objects.filter(id=int(self.test_id))
-            self.fields['test'] = forms.ModelChoiceField(queryset=tests_set, empty_label=None)
-
 class QuestionAdmin(admin.ModelAdmin):
     model = QuestionPrototype
     inlines = [AnswerInline]
-    form = QuestionForm
+    exclude = ('test',)
 
-    def add_view(self, request, form_url='', extra_context=None):
-        test_id = request.GET.get('test_id')
-        self.form.test_id = test_id
-        #print('test_id:'+str(test_id))
-        return super(QuestionAdmin, self).add_view(request, form_url, extra_context)
-
-    def change_view(self, request, object_id, extra_context=None):
-        self.form.test_id = QuestionPrototype.objects.get(id=object_id).test.id
-        return super(QuestionAdmin, self).change_view(request, object_id, extra_context)
-
-    def response_add(self, request, obj, post_url_continue=None):
-        return HttpResponseRedirect('../../questionprototype/add?test_id=%s' % str(self.form.test_id))
+    def response_change(request, obj):
+        return HttpResponseRedirect('../../testprototype/%s' % str(obj.test.id))
 
 class QuestionInline(admin.StackedInline):
     model = QuestionPrototype
@@ -54,7 +32,6 @@ class TestAdmin(admin.ModelAdmin):
         return super(TestAdmin, self).change_view(request, object_id, form_url, extra_context)
 
     def response_add(self, request, obj, post_url_continue=None):
-        #return HttpResponseRedirect('../../questionprototype/add?test_id=%s' % str(obj.id))
         return HttpResponseRedirect('../%s' % str(obj.id))
 
     def delete_model(self, request, obj):

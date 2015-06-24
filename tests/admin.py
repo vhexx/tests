009f0ce732174109fa68d4061047e1498f371c1a
unknown <profgansmok@gmail.com>
2015-06-24 14:20:17 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -28,7 +28,7 @@ class TestAdmin(admin.ModelAdmin):
         return super(TestAdmin, self).add_view(request, form_url, extra_context)
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
-        self.inlines = [QuestionInline, AnswerInline]
+        self.inlines = [QuestionInline]
         return super(TestAdmin, self).change_view(request, object_id, form_url, extra_context)
 
     def response_add(self, request, obj, post_url_continue=None):

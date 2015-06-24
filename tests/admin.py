72e3516eee1fb16b89b586ec9c418690a0de5843
unknown <profgansmok@gmail.com>
2015-06-24 14:37:13 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -21,6 +21,14 @@ class QuestionInline(admin.StackedInline):
     model = QuestionPrototype
     template = 'question_form.html'
 
+class AnswerInline1(admin.StackedInline):
+    model = AnswerPrototype
+    fields = ('question', 'statement', 'test')
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "test":
+            kwargs["queryset"] = TestPrototype.objects.all()
+        return super(MyModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 class TestAdmin(admin.ModelAdmin):
     model = TestPrototype
     def add_view(self, request, form_url='', extra_context=None):
@@ -28,7 +36,7 @@ class TestAdmin(admin.ModelAdmin):
         return super(TestAdmin, self).add_view(request, form_url, extra_context)
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
-        self.inlines = [QuestionInline]
+        self.inlines = [QuestionInline, AnswerInline1]
         return super(TestAdmin, self).change_view(request, object_id, form_url, extra_context)
 
     def response_add(self, request, obj, post_url_continue=None):

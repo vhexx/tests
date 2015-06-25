db1254e05f20115cfef4272bde451b41202a5bae
unknown <profgansmok@gmail.com>
2015-06-25 18:12:09 +0300

    cmt

--- tests/admin.py
+++ tests/admin.py
@@ -35,8 +35,11 @@ class QuestionForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(QuestionForm, self).__init__(*args, **kwargs)
         if QuestionForm.last: # автозаполнения поля order
-            #QuestionForm.last += 1
-            self.fields['order'].initial = self.instance.order #QuestionForm.last
+            default_question = Question()
+            default_order = default_question.order
+            if (self.instance.order == default_order):
+                QuestionForm.last += 1
+                self.fields['order'].initial = QuestionForm.last
 
 
 class PreQuestionAdmin(admin.ModelAdmin):

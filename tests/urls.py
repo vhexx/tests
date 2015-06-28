5c5931a04d73c4f9858382824575c62e5e5f4620
unknown <profgansmok@gmail.com>
2015-06-28 17:34:15 +0300

    fix urls.py, admin.py

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tests import views
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
@@ -9,3 +10,9 @@ urlpatterns = patterns('',
     url(r'^postquestion/(?P<question_id>[0-9]*)/$', views.postquestion),
     url(r'^$', views.index),
 )
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )

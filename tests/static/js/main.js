04f18cd54b134406088e17c4b3b86d5e66679e3a
unknown <profgansmok@gmail.com>
2015-07-06 02:11:24 +0300

    change main.js, main_style.css, image_pair.html

--- tests/static/js/main.js
+++ tests/static/js/main.js
@@ -3,25 +3,7 @@ $(window).on('load', function () {
   $('#main_form div').css({'opacity' : '1.0'});
 });
 $(document).on('ready', function () {
-  var pair = {
-    input_left : $('.img_choice_area input.left'),
-    img_left : $('.img_choice_area img.left'),
-    input_right : $('.img_choice_area input.right'),
-    img_right : $('.img_choice_area img.right')
-  };
-  function switch_img (a, b)
-  {
-    pair['input_'+a].attr({'checked' : 'checked'});
-    pair['img_'+a].css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
-    pair['input_'+b].removeAttr('checked');
-    pair['img_'+b].css({'box-shadow' : 'none'});
-  }
   $('.img_choice_area img').on('click', function () {
-    if ($(this).hasClass('left')) {
-      switch_img('left', 'right');
-    }
-    else {
-      switch_img('right', 'left');
-    }
+    $(this).css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
   });
 });

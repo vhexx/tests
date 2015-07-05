13ac7fabc6967e80782afc0c8c5e50997a42c1c5
unknown <profgansmok@gmail.com>
2015-07-06 02:35:05 +0300

    change main.js

--- tests/static/js/main.js
+++ tests/static/js/main.js
@@ -6,4 +6,20 @@ $(document).on('ready', function () {
   $('.img_choice_area img').on('click', function () {
     $(this).css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
   });
+  var img_blocks = $('.img_div');
+  $(document).keydown(function (event) {
+    switch(event.which)
+    {
+      case 37:
+        img_blocks.eq(0).css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
+        setTimeout(function() {
+          img_blocks.eq(0).submit();
+        }, 100);
+      case 39:
+        img_blocks.eq(1).css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
+        setTimeout(function() {
+          img_blocks.eq(1).submit();
+        }, 100);
+    }
+  });
 });

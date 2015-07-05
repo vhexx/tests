134c13a6b17f960a920be3cf8f55a1bfd0966319
unknown <profgansmok@gmail.com>
2015-07-05 22:23:12 +0300

    change main.js

--- tests/static/js/main.js
+++ tests/static/js/main.js
@@ -1,18 +1,27 @@
 $(window).on('load', function () {
   $('#loader').remove();
-  $('#main_form').css({'display' : 'block'})
+  $('#main_form div').css({'opacity' : '1.0'});
 });
 $(document).on('ready', function () {
-  var left = $('.img_choice_area').find('input.left');
-  var right = $('.img_choice_area').find('input.right');
+  var pair = {
+    input_left : $('.img_choice_area input.left'),
+    img_left : $('.img_choice_area img.left'),
+    input_right : $('.img_choice_area input.right'),
+    img_right : $('.img_choice_area img.right')
+  };
+  function switch_img (a, b)
+  {
+    pair['input_'+a].attr({'checked' : 'checked'});
+    pair['img_'+a].css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
+    pair['input_'+b].removeAttr('checked');
+    pair['img_'+b].css({'box-shadow' : 'none'});
+  }
   $('.img_choice_area img').on('click', function () {
     if ($(this).hasClass('left')) {
-      left.attr({'checked' : 'checked'});
-      right.removeAttr('checked');
+      switch_img('left', 'right');
     }
     else {
-      left.removeAttr('checked');
-      right.attr({'checked' : 'checked'});
+      switch_img('right', 'left');
     }
   });
 });

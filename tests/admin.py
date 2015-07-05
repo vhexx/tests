1cac0025ef15b0cec0aaad345e5248264efbbcd4
unknown <profgansmok@gmail.com>
2015-07-05 22:58:37 +0300

    change admin.py

--- tests/admin.py
+++ tests/admin.py
@@ -43,7 +43,7 @@ class AnswerInline(admin.StackedInline):
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = (('separator'), ('title', 'order', 'type', ), )
+        fields = ('separator', 'title', 'order', 'type',)
 
     separator = forms.ChoiceField(choices=['yes', 'no']);
     last = None

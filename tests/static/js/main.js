6e87febecbe9c94bcaa804d0d0b2957019f7adfe
unknown <profgansmok@gmail.com>
2015-07-06 17:44:42 +0300

    change css, js, image_pair.html

--- tests/static/js/main.js
+++ tests/static/js/main.js
@@ -3,15 +3,12 @@ $(window).on('load', function () {
   $('#main_form div').css({'opacity' : '1.0'});
 });
 $(document).on('ready', function () {
-  $('.img_choice_area img').on('click', function () {
-    $(this).css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
-  });
   var left_button = $('#left');
   var right_button = $('#right');
   function btnClick(btn)
   {
     btn.closest('.img_div').find('img').css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
-    setInterval(function() {btn[0].click();}, 100);
+    setTimeout(function() {btn[0].click();}, 100);
   }
   $(document).keydown(function (event) {
     switch(event.which)

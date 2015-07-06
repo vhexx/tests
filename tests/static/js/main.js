d2e29928f0c181cf9a5dd501b8ea14299568b6be
unknown <profgansmok@gmail.com>
2015-07-06 19:31:58 +0300

    change templates, static

--- tests/static/js/main.js
+++ tests/static/js/main.js
@@ -3,19 +3,24 @@ $(window).load(function () {
   $('#main_form div').css({'opacity' : '1.0'});
   var left_button = $('#left');
   var right_button = $('#right');
+  var button_pressed = false;
   function btnClick(btn)
   {
     btn.closest('.img_div').find('img').css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
     setTimeout(function() {btn[0].click();}, 100);
   }
   $(document).keydown(function (event) {
-    switch(event.which)
-    {
+    if (! button_pressed) {
+      switch(event.which)
+      {
       case 37:
         btnClick(left_button);
+        button_pressed = true;
         break;
       case 39:
         btnClick(right_button);
+        button_pressed = true;
+      }
     }
   });
   $('.img_div img').on('click', function() {

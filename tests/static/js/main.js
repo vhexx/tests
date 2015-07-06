5471809c42df3b3badccf8e22a66c95677751bd5
unknown <profgansmok@gmail.com>
2015-07-06 15:35:09 +0300

    change main.js, image_pair.html

--- tests/static/js/main.js
+++ tests/static/js/main.js
@@ -7,19 +7,17 @@ $(document).on('ready', function () {
     $(this).css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
   });
   var img_blocks = $('.img_div');
+  var left_form = $('#left_form');
+  var right_form = $('#right_form');
   $(document).keydown(function (event) {
     switch(event.which)
     {
       case 37:
         img_blocks.eq(0).css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
-        setTimeout(function() {
-          img_blocks.eq(0).find('form').eq(0).submit();
-        }, 100);
+        left_form.submit();
       case 39:
         img_blocks.eq(1).css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
-        setTimeout(function() {
-          img_blocks.eq(1).find('form').eq(0).submit();
-        }, 100);
+        right_form.submit();
     }
   });
 });

b13c0458ecca547aa2813925c58a63429504c685
unknown <profgansmok@gmail.com>
2015-07-06 16:15:03 +0300

    change image_pair.html, main.js

--- tests/static/js/main.js
+++ tests/static/js/main.js
@@ -6,17 +6,16 @@ $(document).on('ready', function () {
   $('.img_choice_area img').on('click', function () {
     $(this).css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
   });
-  var left_form = $('#left_form');
-  var right_form = $('#right_form');
+  var left_button = $('#left_button');
+  var right_button = $('#right_button');
   $(document).keydown(function (event) {
     switch(event.which)
     {
       case 37:
-        left_form.closest('.img_div').css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
-        left_form[0].submit();
+        left_button.closest('.img_div').css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
+        left_button[0].click();
       case 39:
-        right_form.closest('.img_div').css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
-        right_form[0].submit();
-    }
+        right_button.closest('.img_div').css({'box-shadow' : '0px 0px 6px 3px rgb(54, 141, 218)'});
+        right_button[0].click();
   });
 });

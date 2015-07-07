7cde0df87eb35d01756d9ec4f49581e07a2fd447
unknown <profgansmok@gmail.com>
2015-07-07 04:42:04 +0300

    change admin.py

--- tests/admin.py
+++ tests/admin.py
@@ -154,7 +154,7 @@ class TestForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(TestForm, self).__init__(*args, **kwargs)
-        if self.test_obj_id:
+        if self.test_url:
             self.fields['link'].initial = test_url
         self.fields['link'].widget.attrs['readonly'] = True
 

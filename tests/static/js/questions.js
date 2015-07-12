a994228e562c3ff3259aee73409f570e64057b2f
unknown <profgansmok@gmail.com>
2015-07-13 00:37:13 +0300

    change templates, js

--- tests/static/js/questions.js
+++ tests/static/js/questions.js
@@ -1,5 +1,6 @@
 function checkboxes_valid(element)
 {
+	var res = true;
 	var checkboxes = element.find('input[type="checkbox"]');
 	var checkbox_names_checked = {}, name='', checked_count = 0, len = checkboxes.length;
 	for (var i=0; i<len; i++) {
@@ -8,37 +9,42 @@ function checkboxes_valid(element)
 			checked_count = element.find('input[type="checkbox"][name="'+name+'"]:checked').length;
 			if (checked_count > 0) {
 				checkbox_names_checked[name] = checked_count;
+				$('#'+name+'_question .required').css({'display' : 'none'});
 			}
 			else {
-				return false;
+				$('#'+name+'_question .required').css({'display' : 'inline'});
+				res = false;
 			}
 		}
 	}
-	return true;
+	return res;
 }
 function radios_valid(element)
 {
+	var res = true;
 	var radios = element.find('input[type="radio"]');
 	var len = radios.length, name='', radio_names_checked={};
 	for (var i=0; i<len; i++) {
 		name = radios.eq(i).attr('name');
 		if (!(name in radio_names_checked)) {
 		    if (element.find('input[type="radio"][name="'+name+'"]:checked').length == 0) {
-			    return false;
+			    $('#'+name+'_question .required').css({'display' : 'inline'});
+			    res = false;
 		    }
 		    else {
 			    radio_names_checked[name] = 1;
+			    $('#'+name+'_question .required').css({'display' : 'none'});
 		    }
 		}
 	}
-	return true;
+	return res;
 }
 
 $(document).ready(function() {
 	var main_form = $('#main_form');
 	$('#next').closest('form').submit(function(eventObject) {
 		if ( !( checkboxes_valid(main_form) && radios_valid(main_form) ) ) {
-			alert('Пожалуйста, дайте ответ на все вопросы');
+			alert('Пожалуйста, ответьте на все вопросы анкеты.');
 			eventObject.preventDefault();
 		}
 	});
